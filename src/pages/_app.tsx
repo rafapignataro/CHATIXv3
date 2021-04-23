cc9c94535e8d0b32718037c7bc27bb696f022c70
rafaelpignataro <rafael@mswebmaker.com>
2021-04-23 16:33:14 -0300

    feat: Prettier & Chakra-UI configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,16 @@ import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
 
 import { ThemeProvider } from 'next-themes';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
+    <ChakraProvider theme={theme}>
       <div className="h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-50 relative">
         <Component {...pageProps} />
       </div>
-    </ThemeProvider>
+    </ChakraProvider>
   );
 }
 
